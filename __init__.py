2a475ec9fa8f35ea058e5758b2bb927fc8e05aea
homelyseven250 <61860079+homelyseven250@users.noreply.github.com>
2020-06-15 06:12:32 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -28,14 +28,14 @@ class YoutubeMusic(MycroftSkill):
                             optional data(dict))
                      or None if no match was found.
         """
-        # Get match and confidence
+        
         urls = []
         titles = []
         track_dict = {}
-        self.log.info(phrase.data.get())
-        responce = urllib.request.urlopen('https://www.youtube.com/results?search_query='+phrase.data.get())
+        self.log.info(phrase)
+        response = urllib.request.urlopen('https://www.youtube.com/results?search_query='+phrase)
 
-        soup = BeautifulSoup(responce)    
+        soup = BeautifulSoup(response)    
         divs = soup.find_all("div", { "class" : "yt-lockup-content"})
 
 
